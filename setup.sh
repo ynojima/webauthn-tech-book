ecfaf8bda4721789c138c82de940f8e287c8b7eb
Wataru Haniyama <haniyama_wataru@sgk.jp>
2018-08-27 11:01:24 +0900

    add no-bin-links option for windows docker environment

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ set -eux
 
 rm -rf node_modules
 # --unsafe-perm はrootでの実行時(= docker環境)で必要 非root時の挙動に影響なし
-npm install --unsafe-perm
+# Windows x docker-machine 環境下でシンボリックリンクが作れないため --no-bin-links オプションを追加
+npm install --unsafe-perm --no-bin-links
 git submodule init && git submodule update
